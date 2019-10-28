bcd14971c17ebc60262354a9eba62b1cb8a174c2
mk688689 <55427293+mk688689@users.noreply.github.com>
2019-10-28 15:23:51 +0530

    hsjdhs

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from datetime import date
 
 class Blog(models.Model):
     title = models.CharField('post title', max_length=200)
-    image = models.ImageField (upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     message = models.TextField(default="")
     description = models.TextField()
     posted_date = models.DateField(default=date.today)
